1167a76f41794b5b40f3b12c6718f7c6c1f78f9e
alab <araki@logvox.jp>
2026-01-04 05:14:50 +0900

    frontend: trigger ondemand and poll results

--- site/app.js
+++ site/app.js
@@ -14,6 +14,7 @@ const LABEL_COLOR = {
 };
 
 const $ = (sel) => document.querySelector(sel);
+const ONDEMAND_ENDPOINT = "https://yt-ondemand.araki-69c.workers.dev/ondemand";
 
 const state = {
   index: null,
